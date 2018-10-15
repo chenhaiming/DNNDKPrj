aa33ebf1ada21110f838052bf13cfdfaf98a24a6
chenhaimingsel <784934471@qq.com>
2018-10-15 23:32:00 +0800

    committe dynamic method for jni test

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,8 @@
 #include <jni.h>
 #include <string>
-
+#include "android/log.h"
+#include "logger.h"
+JavaVM *_vm;
 extern "C"
 JNIEXPORT jstring
 
@@ -8,6 +10,31 @@ JNICALL
 Java_comqh_chm_dnndkprj_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "I m is on Jni onLoad";
     return env->NewStringUTF(hello.c_str());
+}
+
+void dynamicTest(){
+    LOGE("JNI dynamicTest");
+}
+static const char *mClassName = "comqh/chm/dnndkprj/MainActivity";
+JNINativeMethod method[] = {
+        {"dynamicJavaTest","()V",(void *)dynamicTest}
+};
+
+int JNI_OnLoad(JavaVM *vm, void *re){
+    //
+    _vm = vm;
+    // 获得JNIEnv
+    JNIEnv *env = 0;
+    // 小于0 失败 ，等于0 成功
+    int r = vm->GetEnv((void**)&env,JNI_VERSION_1_2);
+    if (r != JNI_OK){
+        return -1;
+    }
+    //获得 class对象
+    jclass jcls =  env->FindClass(mClassName);
+    //注册
+    env->RegisterNatives(jcls,method, sizeof(method)/ sizeof(JNINativeMethod));
+    return JNI_VERSION_1_2;
 }
--- app/src/main/java/comqh/chm/dnndkprj/MainActivity.java
+++ app/src/main/java/comqh/chm/dnndkprj/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+        dynamicJavaTest();
     }
 
     /**
@@ -26,4 +27,6 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public native void dynamicJavaTest();
+
 }
